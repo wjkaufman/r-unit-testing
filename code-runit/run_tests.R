9af91a48dc3cd944fbaca6a939a4548905008a14
Will Kaufman <wjkhoops@gmail.com>
2016-07-06 13:41:32 -0600

    update runit run_tests.R

--- code-runit/run_tests.R
+++ code-runit/run_tests.R
@@ -1,4 +1,4 @@
-setwd("~/Google-Drive-UCAR/Will_Kaufman_NCAR/unit-testing/code/")
+setwd("~/Google-Drive-UCAR/Will_Kaufman_NCAR/unit-testing/code-runit/")
 library(RUnit)
 
 source("sample.R")
@@ -8,4 +8,3 @@ testsuite.c2f <- defineTestSuite(name = "exampleSuite",
 
 test.result <- runTestSuite(testsuite.c2f)
 printTextProtocol(test.result)
-# printHTMLProtocol(test.result)
